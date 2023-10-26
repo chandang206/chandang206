f53c04ce913f50aea36c251307d6f8283e8cd075
chandang206 <110524377+chandang206@users.noreply.github.com>
2023-10-26 19:16:26 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+//Keep header background static on scroll
+
+let header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+    header.classList.toggle("shadow", window.scrollY > 0);
+});
